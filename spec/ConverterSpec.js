16e3bdf351c2f46aaf98d5f6237d01a3de385e7c
Carla G. Chiodi <carla.chiodi@gmail.com>
2021-07-04 18:58:41 +0100

    other converting functions

--- spec/ConverterSpec.js
+++ spec/ConverterSpec.js
@@ -4,6 +4,28 @@ describe("Cooking Ingredients Converter", function() {
     it("Fahrenheit to Celsius", function() {
       var result = convertFahrenheitToCelsius(100);
       expect(result).toEqual('37.8');
+      expect(convertFahrenheitToCelsius(50)).toEqual('10.0');
+    });
+    it("Celsius to Fahrenheit", function() {
+      expect(convertCelsiusToFahrenheit(30)).toEqual('86.0');
+    });
+  });
+
+  describe("Weights", function() {
+    it("Pounds to Kilos", function() {
+      expect(convertPoundsToKilos(10)).toEqual('4.5');
+    });
+  });
+
+  describe("Volumes", function() {
+    it("Litres to Gallons", function() {
+      expect(convertLitresToGallons(305)).toEqual('67.1');
+    });
+  });
+
+  describe("Distances", function() {
+    it("Miles to Kilometers", function() {
+      expect(convertMilesToKm(72)).toEqual('115.8');
     });
   });
 });
--- src/Converter.js
+++ src/Converter.js
@@ -4,3 +4,23 @@ function convertFahrenheitToCelsius(temperature) {
   let celsius = (temperature - 32)/1.8
   return celsius.toFixed(1);
 }
+
+function convertCelsiusToFahrenheit(temperature) {
+  let fahrenheit = temperature * 1.8 + 32
+  return fahrenheit.toFixed(1);
+}
+
+function convertPoundsToKilos(pounds) {
+  let kilos = pounds * 0.4536
+  return kilos.toFixed(1);
+}
+
+function convertLitresToGallons(litres) {
+  let gallons = litres * 0.22
+  return gallons.toFixed(1);
+}
+
+function convertMilesToKm(miles) {
+  let km = miles * 1.609
+  return km.toFixed(1);
+}
