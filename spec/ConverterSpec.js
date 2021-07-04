de6ca67a5ef1a0a18557767a3e326bb0dedd6bc3
Carla G. Chiodi <carla.chiodi@gmail.com>
2021-07-04 18:15:13 +0100

    making test work

--- spec/ConverterSpec.js
+++ spec/ConverterSpec.js
@@ -3,7 +3,6 @@ describe("Cooking Ingredients Converter", function() {
   describe("Temperatures", function() {
     it("Fahrenheit to Celsius", function() {
       var result = convertFahrenheitToCelsius(100);
-
       expect(result).toEqual('37.8');
     });
   });
--- src/Converter.js
+++ src/Converter.js
@@ -1,4 +1,6 @@
 /** Unit Converter **/
 function convertFahrenheitToCelsius(temperature) {
   //conversion code
+  let celsius = (temperature - 32)/1.8
+  return celsius.toFixed(1);
 }
